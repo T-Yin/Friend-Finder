ba9a203467a5cd958a31bfd52865ba7efd99cf6b
Tiffany ✪ Starrii <xtiffany.yin@gmail.com>
2019-02-24 23:28:33 -0700

    add files

---
+++ app/data/friends.js
@@ -0,0 +1,75 @@
+var friends =
+    [
+        {
+            "name": "Spongebob",
+            "photo": "../css/images/spongebob.jpg",
+            "scores": [
+                1, 4, 5, 5, 3, 5, 4, 5, 2, 2
+            ]
+        },
+        {
+            "name": "Patrick",
+            "photo": "../css/images/patrick.jpg",
+            "scores": [
+                1, 5, 5, 5, 2, 2, 1, 3, 3, 5
+            ]
+        },
+        {
+            "name": "Squidward",
+            "photo": "../css/images/squidward.jpg",
+            "scores": [
+                3, 2, 1, 1, 1, 2, 4, 5, 5, 3
+            ]
+        },
+        {
+            "name": "Mr. Krabs",
+            "photo": "../css/images/mrkrabs.jpg",
+            "scores": [
+                4, 5, 2, 2, 1, 2, 5, 4, 4, 5
+            ]
+        },
+        {
+            "name": "Plankton",
+            "photo": "../css/images/plankton.jpg",
+            "scores": [
+                5, 5, 1, 1, 2, 1, 5, 5, 2, 1
+            ]
+        },
+        {
+            "name": "Karen",
+            "photo": "../css/images/karen.jpg",
+            "scores": [
+                4, 2, 3, 3, 3, 2, 4, 2, 5, 5
+            ]
+        },
+        {
+            "name": "Sandy",
+            "photo": "../css/images/sandy.jpg",
+            "scores": [
+                2, 4, 4, 4, 4, 3, 4, 3, 1, 4
+            ]
+        },
+        {
+            "name": "Pearl",
+            "photo": "../css/images/pearl.jpg",
+            "scores": [
+                2, 2, 4, 5, 2, 4, 3, 5, 2, 4
+            ]
+        },
+        {
+            "name": "Mrs. Puff",
+            "photo": "../css/images/mrspuff.jpg",
+            "scores": [
+                5, 1, 4, 2, 5, 3, 2, 1, 5, 5
+            ]
+        },
+        {
+            "name": "Gary",
+            "photo": "../css/images/gary.jpg",
+            "scores": [
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+            ]
+        }
+    ]
+
+module.exports = friends
